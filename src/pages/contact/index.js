26888a0fc1a0e4e39c77290f9c1870bb69b46401
Alesger <101628080+Elesgerr@users.noreply.github.com>
2024-04-13 15:09:52 +0400

    gtag problem

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -4,12 +4,14 @@ import Head from "next/head";
 import React from "react";
 
 const ContactPage = () => {
+   let GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
   return (
     <>
       <Head>
         <title>Contact Page - Asgofy</title>
       </Head>
       <div>
+        <span>{GTM_ID}</span>
         <ContactForm />
       </div>
       {/* <section class="bg-white dark:bg-gray-900">
