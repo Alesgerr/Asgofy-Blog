df0bb2380baa49bde363dfc08da0d290d73dc47d
Alesger <101628080+Elesgerr@users.noreply.github.com>
2024-04-15 18:44:32 +0400

    rss category

--- src/pages/categories/[slug]/rss.xml.js
+++ src/pages/categories/[slug]/rss.xml.js
@@ -1,45 +1,47 @@
-// pages/[category]/rss.xml
-
 import { Feed } from "feed";
 import { getProductsByCategory } from "../../../../sanity/lib/client";
 
-export async function getServerSideProps({ params }) {
+function generateRSSFeed(products) {
+  const feed = new Feed({
+    title: "My Website RSS Feed",
+    description: "Latest articles in the category",
+    link: "https://asgofy.com/",
+    language: "en",
+    // Diğer isteğe bağlı ayarları buraya ekleyebilirsiniz
+  });
+
+  products.forEach((product) => {
+    feed.addItem({
+      title: product.title,
+      id: product._id,
+      link: `https://asgofy.com/blog/${product?.slug}`,
+      description: product?.description,
+      date: new Date(product.publishedAt), // Ürünün yayınlanma tarihini ekleyin
+    });
+  });
+
+  // Oluşturulan feed'i döndürün
+  return feed.rss2();
+}
+
+export async function getServerSideProps({ params, res }) {
   const { slug } = params;
   // Kategoriden ürünleri almak için gerekli işlemleri yapın
   const products = await getProductsByCategory(slug);
 
   // Ürünleri RSS XML formatına dönüştürün
   const feed = generateRSSFeed(products);
- 
-  return {
-    props: {
-      feed,
-      products,
-    },
-  };
+
+  // XML içeriğini döndürmek için HTTP yanıtını yapılandırın
+  res.setHeader("Content-Type", "text/xml");
+  res.write(feed);
+  res.end();
+
+  // Döndürülen içeriği kullanmadığımız için `props` objesini boş bırakabiliriz
+  return { props: {} };
 }
- function generateRSSFeed(products) {
-   const feed = new Feed({
-     title: "My Website RSS Feed",
-     description: "Latest articles in the category",
-     link: "https://asgofy.com/",
-     language: "en",
-     // Diğer isteğe bağlı ayarları buraya ekleyebilirsiniz
-   });
 
-   products.forEach((product) => {
-     feed.addItem({
-       title: product?.title,
-       id: product?._id,
-       link: `https://asgofy.com/blog/${product?.slug}`,
-       description: product?.description,
-       date: new Date(product?.publishedAt), // Ürünün yayınlanma tarihini ekleyin
-     });
-   });
-
-   // Oluşturulan feed'i döndürün
-   return feed.rss2();
- }
-export default function RSS({ feed }) {
-  return feed;
+export default function RSS() {
+  // Bu bileşen aslında hiçbir şey yapmaz, çünkü RSS içeriğini HTTP yanıtı olarak döndürüyoruz
+  return null;
 }
